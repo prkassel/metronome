3c937b2de5784a537c0c637932da32689c0ed63f
prkassel <prkassel@gmail.com>
2015-12-03 21:08:16 -0600

    Add buttons

--- js/index.js
+++ js/index.js
@@ -1,28 +1,44 @@
-var meter = 0
-var bpm = 80; 
-var division = 4; 
+var meter = 0;
+var bpm = 80;
+var division = 4;
 var soundID = "Thunder";
 $(".bpm").html(bpm);
 
 $(document).ready(function() {
 function loadSound() {
-  createjs.Sound.registerSound("http://pkassel.com/sounds/woodblock.mp3", soundID);
+  createjs.Sound.registerSound("https://raw.githubusercontent.com/prkassel/metronome/master/sounds/woodblock.mp3", soundID);
 }
 
 function metronome(bpm) {
-  var milliSec = (60 / bpm) * 1000; 
+  $("#startMet").addClass("hidden");
+  $("#stopMet").removeClass("hidden");
+  var milliSec = (60 / bpm) * 1000;
   var beatTime = function() {
     $(".bpm").toggleClass("red");
      createjs.Sound.play(soundID);
-
-  }
-  var speed = window.setInterval(beatTime, milliSec); 
-
+      
+  };
+    var speed = window.setInterval(beatTime, milliSec);
+  $("#stopMet").click(function() {
+    clearInterval(speed);
+    $("#startMet").removeClass("hidden");
+    $("#stopMet").addClass("hidden");
+  });
 }
 
 $("#startMet").click(function(){
-    metronome(bpm); 
+    metronome(bpm);
   });
   
-  loadSound(); 
+  loadSound();
+  $("#faster").click(function() {
+    bpm = bpm + 1;
+    $(".bpm").html(bpm);
+    return bpm;
+  });
+  $("#slower").click(function() {
+    bpm = bpm - 1;
+    $(".bpm").html(bpm);
+    return bpm;
+  });
 });
