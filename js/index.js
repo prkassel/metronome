c411ddcf5dc2fac373034ecf2329f0513d18d443
prkassel <prkassel@gmail.com>
2015-12-04 19:36:42 -0600

    Improved functionality
    This 2 different woodblock sounds, adjustable meter and improved GUI.

--- js/index.js
+++ js/index.js
@@ -1,22 +1,38 @@
-var meter = 0;
 var bpm = 80;
+var meter = 4;
 var division = 4;
 var soundID="woodblock";
+var downbeat="woodblock1";
+var count = 1;
 $(".bpm").html(bpm);
-
+$(".count").html(count);
+$(".meter").html(meter + " / 4");
 $(document).ready(function() {
 function loadSound() {
-  createjs.Sound.registerSound("https://raw.githubusercontent.com/prkassel/metronome/master/sounds/woodblock.mp3", soundID);
+  createjs.Sound.registerSound("https://raw.githubusercontent.com/prkassel/metronome/master/sounds/woodblock.mp3", downbeat);
+  
+ createjs.Sound.registerSound("https://raw.githubusercontent.com/prkassel/metronome/master/sounds/woodblock1.mp3", soundID);
 }
-
-function metronome(bpm) {
+  
+function metronome(bpm, meter, count) {
   $("#startMet").addClass("hidden");
   $("#stopMet").removeClass("hidden");
   var milliSec = (60 / bpm) * 1000;
+  
   var beatTime = function() {
-    $(".bpm").toggleClass("red");
+     $(".count").html(count);
+    if (count == 1) {
+      createjs.Sound.play(downbeat);
+    }
+    else {
      createjs.Sound.play(soundID);
-      
+    }
+    if (count == meter) {
+      count = (count - meter) + 1;
+    }
+    else if (count < meter) {
+      count ++;
+    }
   };
     var speed = window.setInterval(beatTime, milliSec);
   $("#stopMet").click(function() {
@@ -27,7 +43,7 @@ function metronome(bpm) {
 }
 
 $("#startMet").click(function(){
-    metronome(bpm);
+    metronome(bpm, meter, count);
   });
   
   loadSound();
@@ -41,4 +57,21 @@ $("#startMet").click(function(){
     $(".bpm").html(bpm);
     return bpm;
   });
+  
+  $("#lessMeter").click(function() {
+    if(meter > 1) {
+    meter = meter - 1;
+    $(".meter").html(meter + " / 4");
+    return meter;
+    }
+  });
+  
+  $("#addMeter").click(function() {
+   if(meter < 7) {
+    meter = meter + 1;
+    $(".meter").html(meter + " / 4");
+    return meter;
+   }
+  });
+  
 });
