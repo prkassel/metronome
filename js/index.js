fa14567c328d7a76b7119e9f399d957731d77ef8
prkassel <prkassel@gmail.com>
2015-12-05 11:22:15 -0600

    Change tempo/meter while playing.

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ var division = 4;
 var soundID="woodblock";
 var downbeat="woodblock1";
 var count = 1;
+var playing;
 
 $(document).ready(function() {
 
@@ -20,6 +21,10 @@ $( "#slider" ).slider({
       slide: function( event, ui ) {
         bpm = ui.value;
         $( ".bpm" ).html(ui.value);
+        if (playing) {
+          clearInterval(playing);
+          metronome(bpm, meter, count);
+          }
         return bpm;
       }
     });
@@ -31,9 +36,9 @@ function loadSound() {
 }
   
 function metronome(bpm, meter, count) {
+  var milliSec = (60 / bpm) * 1000;
   $("#startMet").addClass("hidden");
   $("#stopMet").removeClass("hidden");
-  var milliSec = (60 / bpm) * 1000;
   
   var beatTime = function() {
      $(".count").html(count);
@@ -50,9 +55,10 @@ function metronome(bpm, meter, count) {
       count ++;
     }
   };
-    var speed = window.setInterval(beatTime(bpm), milliSec);
+   playing = window.setInterval(beatTime, milliSec);
   $("#stopMet").click(function() {
-    clearInterval(speed);
+    clearInterval(playing);
+    playing = false;
     $("#startMet").removeClass("hidden");
     $("#stopMet").addClass("hidden");
   });
@@ -67,6 +73,10 @@ $("#startMet").click(function(){
     if(meter > 1) {
     meter = meter - 1;
     $(".meter").html(meter + " / 4");
+    if (playing) {
+          clearInterval(playing);
+          metronome(bpm, meter, count);
+          }
     return meter;
     }
   });
@@ -75,6 +85,10 @@ $("#startMet").click(function(){
    if(meter < 7) {
     meter = meter + 1;
     $(".meter").html(meter + " / 4");
+    if (playing) {
+          clearInterval(playing);
+          metronome(bpm, meter, count);
+          }
     return meter;
    }
   });
